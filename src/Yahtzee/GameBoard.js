370b5048181a2816e2c3a60e681bb78fe60d9886
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-06-01 09:25:55 -0500

    Locked dice now held in int & dice can be roled

--- src/Yahtzee/GameBoard.js
+++ src/Yahtzee/GameBoard.js
@@ -1,23 +1,36 @@
 import React, {useState} from 'react';
-import './yahtzee.css';
 import Dice from "./Dice";
 
 const GameBoard = props =>{
-    const initialArray = [];
-    for(let i of props.dice)
-        initialArray.push(false);
-
-    const [locked, setLocked] = useState(initialArray);
+    const [locked, setLocked] = useState(0);
     const flip = index => {
-        console.log(index);
+        let mask = 1 << index;
+        setLocked(v=>v^=mask);
+    }
+
+    const isLocked = index => {
+        let mask = 1 << index;
+        return ( (locked&mask) !== 0);
+    }
+
+    const roleDice = () =>{
+        let dice = [];
+        for(let i in props.dice){
+            if(isLocked(i)){
+                dice.push(props.dice[i]);
+            } else {
+                dice.push( Math.floor(Math.random()*6)+1 );
+            }
+        }
+        props.onRole(dice);
     }
 
     return (
         <>
-            {props.dice.map((value, index)=><Dice value={value} locked={locked[index]} index={index} onClick={flip}/>)}
+            {props.dice.map((value, index)=><Dice value={value} locked={isLocked(index)} index={index} onClick={()=>flip(index)}/>)}
             <section>
-                <button className="button" id="role">Role Dice</button>
-                <button className="button">Reset</button>
+                <button className="button" id="role" onClick={roleDice}>Role Dice</button>
+                <button className="button" onClick={props.onReset}>Reset</button>
             </section>
         </>
     );
