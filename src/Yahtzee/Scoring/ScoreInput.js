e58e9e15a35a422d05686f90b11000bf297b6605
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-06-08 08:38:37 -0500

    swited to recieve promise.

--- src/Yahtzee/Scoring/ScoreInput.js
+++ src/Yahtzee/Scoring/ScoreInput.js
@@ -1,14 +1,13 @@
 import React, {useState} from "react";
 
 const ScoreInput = props => {
-    const [score, setScore] = useState(<button onClick={()=>{
-        const update = value => {
-            setScore(<span className="btn">{value}</span>);
-        }
+    const update = async() => {
+        let newScore = await props.update();
 
-        props.update(update);
-    }}>Take</button>);
-    
+        setScore(<span className='btn'>{newScore}</span>);
+    }
+
+    const [score, setScore] = useState(<button onClick={update}>Take</button>);
     
     return (
         <div className="input">
