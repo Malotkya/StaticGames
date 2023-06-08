865f9af4abfe471db9f079942c3ebf09b8c51905
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-06-08 08:36:55 -0500

    game now keeps track if the dice should be locked

--- src/Yahtzee/index.js
+++ src/Yahtzee/index.js
@@ -1,20 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './yahtzee.css';
 import GameBoard from "./GameBoard";
 import ScoreBoard from "./ScoreBoard";
 
+const MAX_ROLES = 3;
+
 const Yahtzee = () =>{
     const [diceValues, setDice] = useState([1, 2, 3, 4, 5]);
-    const [count, setCount] = useState(0);
+    const [resetKey, setReset] = useState(0);
+    const [roleCount, setCount] = useState(0);
+
     const reset = () => {
         setDice([1,2,3,4,5]);
-        setCount(c=>c+1);
+        setReset(c=>c+1);
+    }
+
+    const roles = dice => {
+        setDice(dice);
+        setCount(c=>c+1); 
+    }
+
+    const lockDice = () => {
+        if(MAX_ROLES>0 && roleCount>=MAX_ROLES){
+            return -1;
+        } else {
+            return roleCount;
+        }
+    }
+
+    const lockScore = () => {
+        return roleCount < 1;
     }
 
     return (
         <>
-            <GameBoard dice={diceValues} onRole={setDice} onReset={reset}/>
-            <ScoreBoard key={count} dice={diceValues} />
+            <GameBoard dice={diceValues} onRole={roles} onReset={reset} roleCount={lockDice()}/>
+            <ScoreBoard key={resetKey} dice={diceValues} onClick={()=>setCount(0)} locked={lockScore()} />
         </>
     );
 }
