b043076ac9941fe16579d176c20f8a48361d80af
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-06-08 09:18:13 -0500

    Can now switch between games

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Yahtzee from './Yahtzee';
+import TicTacToe from "./TicTacToe";
 
 function App() {
+  const [game, setGame] = useState(<Yahtzee />)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Yahtzee />
+        <button onClick={()=>setGame(<Yahtzee />)}>Yahtzee</button>
+        <button onClick={()=>setGame(<TicTacToe />)}>Tic Tac Toe</button>
       </header>
+      <main>
+        {game}
+      </main>
+      <footer>
+        <p>
+          About Me!
+        </p>
+      </footer>
     </div>
   );
 }
