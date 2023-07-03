8875d6de5c9ebf8c8f7539c3db48ef555200cb82
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-07-03 14:35:52 -0500

    Added about me content to footer.

--- src/App.js
+++ src/App.js
@@ -5,26 +5,26 @@ import Yahtzee from './Yahtzee';
 import TicTacToe from "./TicTacToe";
 
 function App() {
-  const [game, setGame] = useState(<Yahtzee />)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <nav>
-          <button onClick={()=>setGame(<Yahtzee />)}>Yahtzee</button>
-          <button onClick={()=>setGame(<TicTacToe />)}>Tic Tac Toe</button>
-        </nav>
-      </header>
-      <main>
-        {game}
-      </main>
-      <footer>
-        <p>
-          About Me!
-        </p>
-      </footer>
-    </div>
-  );
+	const [game, setGame] = useState(<Yahtzee />)
+	return (
+		<div className="App">
+		<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<nav>
+				<button onClick={()=>setGame(<Yahtzee />)}>Yahtzee</button>
+				<button onClick={()=>setGame(<TicTacToe />)}>Tic Tac Toe</button>
+			</nav>
+		</header>
+		<main>
+			{game}
+		</main>
+		<footer>
+			Created By: Alex Malotky<br/>
+            <a target="_blank" href="https://github.com/Malotkya/StaticGames">Github Repo</a> |&nbsp; 
+            <a target="_blank" href="https://Alex.Malotky.net/Portfolio">My Other Work</a>
+		</footer>
+		</div>
+	);
 }
 
 export default App;
