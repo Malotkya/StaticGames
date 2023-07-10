fb147b5ce57901eac1ff57e91d6a46b82251fea6
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-07-10 12:04:14 -0500

    Moved Reset btn to prevent accidental click

--- src/Yahtzee/GameBoard.js
+++ src/Yahtzee/GameBoard.js
@@ -87,11 +87,13 @@ const GameBoard = props =>{
         <>
             <section className="section">
                 <button className="button" id="role" onClick={roleDice}>Role Dice</button>
-                <button className="button" onClick={reset}>Reset</button>
             </section>
             <div id="gameboard" className="section">
                 {props.dice.map((value, index)=><Dice key={index} value={value} locked={isLocked(index)} index={index} onClick={()=>flipLock(index)}/>)}
             </div>
+            <section className="section">
+                <button className="button" onClick={reset}>Reset</button>
+            </section>
         </>
     );
 }
